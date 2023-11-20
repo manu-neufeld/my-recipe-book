429e46c70ae1d0c5851fffbd584a965119d089ad
manuela <manuela.neufeld@gmail.com>
2023-11-20 13:48:51 +0100

    ingredient model create

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './shopping-list.component.css'
 })
 export class ShoppingListComponent {
+  ingredients: Ingredient[] = [
+    new Ingredient ('Apples', 5),
+    new Ingredient ('Tomatoes', 5),
+  ];
 
 }
