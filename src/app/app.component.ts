7b112cdbdcfc755f8ec22609d55e564af31aa853
manuela r. neufeld <m.rodriguez@nevintec.com>
2024-01-10 12:17:14 +0100

    navigation added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- 
+  loaderFeature= 'recipe'
+ onNavigate(feature: string){
+  this.loaderFeature = feature
+ }
 }
